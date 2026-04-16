6ca94d3438753a07c4008651c9529d112c4e048b
Alfred Fantone <alfredrick.fantone@edu.sait.ca>
2026-04-16 11:29:30 -0600

    Enable JS Type Checker
    Enable type-checking for `booking.js` and `contact.js`
    using the "// @ts-check" file header. Further Info:
    https://code.visualstudio.com/docs/nodejs/working-with-javascript#_type-checking-javascript
    
    Mainly to add an additional challenge to the
    assignment.
    
    Also, make some minor tweaks.

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,8 +1,12 @@
+// @ts-check
+// ^ Enable Type-Checking with JSDoc Annotations.
+
 /********* create variables *********/
 // useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 
+// Set `35` as the default, since `fullButton` is initially "clicked".
 let costPerDay = 35;
 let numberOfDaysSelected = 0;
 
@@ -13,6 +17,14 @@ const fullButton = document.getElementById("full");
 const halfButton = document.getElementById("half");
 const clearButton = document.getElementById("clear-button");
 const calculatedCost = document.getElementById("calculated-cost");
+
+// Type-checker requires null-checks for outputs of `getElementById()`, 
+// `querySelectorAll()`, and `querySelector()`.
+// Note: The "Not" operator (!) implicitly converts 0 & nullish values (null/undefined) to false.
+// Note 2: Checking for the nullablity of `calculatedCost` here does not tell the 
+//         type-checker that it will not be null in `updateCalculatedCost()`
+if (!dayButtons.length || !fullButton || !halfButton || !clearButton)
+    throw new Error("Required Elements in Booking page does not exist")
 
 
 /********* colour change days of week *********/
@@ -34,6 +46,10 @@ dayButtons.forEach((element) => {
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
 
 clearButton.addEventListener("click", () => {
+    // Avoid unnecessary work ... check whether it's necessary first.
+    if (numberOfDaysSelected == 0)
+        return;
+
     dayButtons.forEach((element) => {
         element.classList.remove("clicked");
         numberOfDaysSelected = 0;
@@ -67,6 +83,11 @@ fullButton.addEventListener("click", () => {
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 
 function updateCalculatedCost() {
+    // Check whether `calculatedCost` is nullish here.
+    // As checking outside the scope of this function does not satisfy the type-checker.
+    if (!calculatedCost)
+        throw new Error("Expected calculated-cost Element does not exist")
+
     const newCost = costPerDay * numberOfDaysSelected;
     /** Convert newCost to string with no decimal places (i.e. integer). */
     calculatedCost.textContent = newCost.toFixed(0);
--- scripts/contact.js
+++ scripts/contact.js
@@ -1,3 +1,6 @@
+// @ts-check
+// ^ Enable Type-Checking with JSDoc Annotations.
+
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
@@ -5,16 +8,21 @@
 const submitButton = document.getElementById("submit-button");
 const contactPage = document.getElementById("contact-page");
 
+// Type-checker requires null-checks for outputs of `getElementById()`.
+// Note: The "Not" operator (!) implicitly converts nullish values to false.
+if (!submitButton || !contactPage)
+    throw new Error("Required Elements in the Contact page does not exist");
+
 submitButton.addEventListener("click", () => {
     // Clear the content of content-page
     contactPage.innerHTML = "";
     
     // Create the new element with the expected text-content 
-    // and font-size (.large-text has 24px font)
+    // and font-size (.large-text has 24px font).
     const message = document.createElement("p");
-    message.textContent = "Thank you for your message";
+    message.textContent = "Thank you for your message!";
     message.classList.add("large-text");
 
-    // Add this created element to the DOM tree.
+    // Add the new element to the DOM tree.
     contactPage.appendChild(message);
 });
